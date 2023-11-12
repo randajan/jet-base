167f44c441d53137a248bef65440534f75383393
Jan Randa <jnranda@gmail.com>
2023-11-12 14:38:49 +0100

    BF: sugar path

--- src/class/Sugar.js
+++ src/class/Sugar.js
@@ -12,5 +12,5 @@ export class Sugar {
 const _methods = ["is", "isType", "isFull", "get", "getType", "set", "remove", "pull", "watch", "fit", "fitTo", "fitType", "setDefault", "setLock"];
 
 for (const m of _methods) {
-    Sugar.prototype[m] = function (path, ...args) { return this.base[m](path ? [this.path, path] : this.path, ...args); }
+    Sugar.prototype[m] = function (path, ...args) { return this.base[m]([this.path, path], ...args); }
 }
